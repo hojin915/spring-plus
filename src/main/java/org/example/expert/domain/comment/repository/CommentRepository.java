4957d0d70575b8b0f0ec7da3df53a661bff8f953
hojin <hojin915@naver.com>
2025-06-24 17:55:40 +0900

    Lv6~Lv7: Todo Cascade설정, getComments N+1문제해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.repository;
 
 import org.example.expert.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,9 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // EntityGraph 로 user 테이블까지 join 해서 한번에 조회
+    // Lazy 로딩으로 인한 n+1 문제 해결가능
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT c FROM Comment c WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
